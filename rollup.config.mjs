c8fe055501c5e9490f766f4147e4db6f2e7ba4a8
Amarthya K R <amarthya10@gmail.com>
2025-02-24 14:53:26 +0530

    [REFACTOR]: Styles added

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,31 +3,37 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
+import postcss from "rollup-plugin-postcss";
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-    ],
-  },
-  {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: packageJson.main,
+				format: "cjs",
+				sourcemap: true,
+			},
+			{
+				file: packageJson.module,
+				format: "esm",
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			resolve(),
+			commonjs(),
+			typescript({ tsconfig: "./tsconfig.json" }),
+			// NEW
+			postcss(),
+		],
+	},
+	{
+		input: "dist/esm/index.d.ts",
+		output: [{ file: "dist/index.d.ts", format: "esm" }],
+		plugins: [dts()],
+
+		// NEW
+		external: [/\.css$/],
+	},
 ];
